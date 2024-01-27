34c384701a8caac6af9982d56bd46f1abaf905a5
Thomas Antony <mail@thomasantony.com>
2024-01-27 14:57:35 -0800

    Fix spacing

--- run.py
+++ run.py
@@ -16,23 +16,23 @@ if __name__ == '__main__':
     parser.add_argument('--img-path', type=str)
     parser.add_argument('--outdir', type=str, default='./vis_depth')
     parser.add_argument('--encoder', type=str, default='vitl', choices=['vits', 'vitb', 'vitl'])
-    
+
     args = parser.parse_args()
-    
+
     margin_width = 50
     caption_height = 60
-    
+
     font = cv2.FONT_HERSHEY_SIMPLEX
     font_scale = 1
     font_thickness = 2
-    
+
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
+
     depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_{}14'.format(args.encoder)).to(DEVICE).eval()
-    
+
     total_params = sum(param.numel() for param in depth_anything.parameters())
     print('Total parameters: {:.2f}M'.format(total_params / 1e6))
-    
+
     transform = Compose([
         Resize(
             width=518,
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         PrepareForNet(),
     ])
-    
+
     if os.path.isfile(args.img_path):
         if args.img_path.endswith('txt'):
             with open(args.img_path, 'r') as f:
@@ -57,30 +57,30 @@ if __name__ == '__main__':
         filenames = os.listdir(args.img_path)
         filenames = [os.path.join(args.img_path, filename) for filename in filenames if not filename.startswith('.')]
         filenames.sort()
-    
+
     os.makedirs(args.outdir, exist_ok=True)
-    
+
     for filename in tqdm(filenames):
         raw_image = cv2.imread(filename)
         image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
-        
+
         h, w = image.shape[:2]
-        
+
         image = transform({'image': image})['image']
         image = torch.from_numpy(image).unsqueeze(0).to(DEVICE)
-        
+
         with torch.no_grad():
             depth = depth_anything(image)
-        
+
         depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
         depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-        
+
         depth = depth.cpu().numpy().astype(np.uint8)
         depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
-        
+
         split_region = np.ones((raw_image.shape[0], margin_width, 3), dtype=np.uint8) * 255
         combined_results = cv2.hconcat([raw_image, split_region, depth_color])
-        
+
         caption_space = np.ones((caption_height, combined_results.shape[1], 3), dtype=np.uint8) * 255
         captions = ['Raw image', 'Depth Anything']
         segment_width = w + margin_width
@@ -93,8 +93,9 @@ if __name__ == '__main__':
 
             # Add text caption
             cv2.putText(caption_space, caption, (text_x, 40), font, font_scale, (0, 0, 0), font_thickness)
-        
+
         final_result = cv2.vconcat([caption_space, combined_results])
-        
+
         filename = os.path.basename(filename)
         cv2.imwrite(os.path.join(args.outdir, filename[:filename.rfind('.')] + '_img_depth.png'), final_result)
+        cv2.imwrite(os.path.join(args.outdir, filename[:filename.rfind('.')] + '_depth.png'), depth)
--- run_video.py
+++ run_video.py
@@ -15,18 +15,18 @@ if __name__ == '__main__':
     parser.add_argument('--video-path', type=str)
     parser.add_argument('--outdir', type=str, default='./vis_video_depth')
     parser.add_argument('--encoder', type=str, default='vitl', choices=['vits', 'vitb', 'vitl'])
-    
+
     args = parser.parse_args()
-    
+
     margin_width = 50
 
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
+
     depth_anything = DepthAnything.from_pretrained('LiheYoung/depth_anything_{}14'.format(args.encoder)).to(DEVICE).eval()
-    
+
     total_params = sum(param.numel() for param in depth_anything.parameters())
     print('Total parameters: {:.2f}M'.format(total_params / 1e6))
-    
+
     transform = Compose([
         Resize(
             width=518,
@@ -51,44 +51,44 @@ if __name__ == '__main__':
         filenames = os.listdir(args.video_path)
         filenames = [os.path.join(args.video_path, filename) for filename in filenames if not filename.startswith('.')]
         filenames.sort()
-    
+
     os.makedirs(args.outdir, exist_ok=True)
-    
+
     for k, filename in enumerate(filenames):
         print('Progress {:}/{:},'.format(k+1, len(filenames)), 'Processing', filename)
-        
+
         raw_video = cv2.VideoCapture(filename)
         frame_width, frame_height = int(raw_video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(raw_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
         frame_rate = int(raw_video.get(cv2.CAP_PROP_FPS))
         output_width = frame_width * 2 + margin_width
-        
+
         filename = os.path.basename(filename)
         output_path = os.path.join(args.outdir, filename[:filename.rfind('.')] + '_video_depth.mp4')
         out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), frame_rate, (output_width, frame_height))
-        
+
         while raw_video.isOpened():
             ret, raw_frame = raw_video.read()
             if not ret:
                 break
-            
+
             frame = cv2.cvtColor(raw_frame, cv2.COLOR_BGR2RGB) / 255.0
-            
+
             frame = transform({'image': frame})['image']
             frame = torch.from_numpy(frame).unsqueeze(0).to(DEVICE)
-            
+
             with torch.no_grad():
                 depth = depth_anything(frame)
 
             depth = F.interpolate(depth[None], (frame_height, frame_width), mode='bilinear', align_corners=False)[0, 0]
             depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0
-            
+
             depth = depth.cpu().numpy().astype(np.uint8)
             depth_color = cv2.applyColorMap(depth, cv2.COLORMAP_INFERNO)
-            
+
             split_region = np.ones((frame_height, margin_width, 3), dtype=np.uint8) * 255
             combined_frame = cv2.hconcat([raw_frame, split_region, depth_color])
-            
+
             out.write(combined_frame)
-        
+
         raw_video.release()
         out.release()
